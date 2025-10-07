d3e200b6597e878bcb3117b7e6c025e8c1004a5a
Gordon <GordonM04126@gmail.com>
2025-10-07 13:47:55 -0700

    updated formatting

--- src/app/page.js
+++ src/app/page.js
@@ -8,19 +8,23 @@ export default function Home() {
     <main className="p-10 pl-16 bg-gradient-to-b from-teal-400 via-cyan-500 to-blue-600"> 
       <Header/>
       <div className="flex flex-row place-content-center gap-20 pb-20 flex-wrap">
-
-      <a href="https://github.com/LexTruong/ZotterMeals">
-        <ProjectCard imgsrc="phone.png" altText="phone" title="ZotterMeals" 
+        <a href="https://github.com/LexTruong/ZotterMeals">
+          <ProjectCard 
+            imgsrc="phone.png" 
+            altText="phone" 
+            title="ZotterMeals" 
             description = "Mobile calorie tracking app integrated with UCI dining information with ReactNative, JS, and, Expo"/>
-      </a>
+        </a>
 
-      <a href="https://devpost.com/software/quick-release-pit-stop">
-        <ProjectCard imgsrc="graph.png" altText="graph" title="Quick Release Pit Stop" 
+        <a href="https://devpost.com/software/quick-release-pit-stop">
+          <ProjectCard 
+            imgsrc="graph.png" 
+            altText="graph" 
+            title="Quick Release Pit Stop" 
             description = "Organized and visualized data through pandas, plot.ly, and seaborn to identify points of interest."/>
-      </a>
-
+        </a>
       </div>      
-    <Footer/>
+      <Footer/>
     </main>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,20 @@
 export default function Footer() {
     return (
-        <footer>
+      <footer>
         <div className="flex flex-row space-x-8 pl-6">
-        <a href="https://www.linkedin.com/in/gordon-ma8/">
-          <img className="hover:scale-105 h-12 w-12 cursor-default hover:cursor-pointer" src="linkedin.png" alt="linkedin"/>
-        </a> 
-        <a href="https://github.com/Gordon-M">
-          <img className="hover:scale-105 h-12 w-12 cursor-default hover:cursor-pointer" src="github.png" alt="github"/>
-        </a>
-      </div>
-        </footer>
+          <a href="https://www.linkedin.com/in/gordon-ma8/">
+            <img 
+              className="hover:scale-105 h-12 w-12 cursor-default hover:cursor-pointer" 
+              src="linkedin.png" 
+              alt="linkedin"/>
+          </a> 
+          <a href="https://github.com/Gordon-M">
+            <img 
+              className="hover:scale-105 h-12 w-12 cursor-default hover:cursor-pointer" 
+              src="github.png" 
+              alt="github"/>
+          </a>
+        </div>
+      </footer>
     )
 }
